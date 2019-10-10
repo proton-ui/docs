c2796a398cd638bd62a5b085030c0b4a05606f93
Cameron Van Orman <cameron@efelle.com>
2019-10-09 20:06:39 -0700

    Adding Href and To for Button examples

---
+++ resources/js/examples/Button/To.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <p-button to="/documentation/components/button#to">Router Link String</p-button>
+        <p-button :to="{ name: '/documentation/components/button', hash: '#to' }">Router Link Object</p-button>
+    </section>
+</template>
